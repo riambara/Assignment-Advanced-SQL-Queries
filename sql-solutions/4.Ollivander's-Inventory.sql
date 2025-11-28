a623abd338f85eb0366cfc40e049c41d25903250
Riambara Abdillah Hazazi <132339405+riambara@users.noreply.github.com>
2025-11-28 21:19:48 +0700

    Create 4.Ollivander's-Inventory.sql

---
+++ sql-solutions/4.Ollivander's-Inventory.sql
@@ -0,0 +1,22 @@
+
+
+
+SELECT id, age, coins_needed, power
+FROM (
+    SELECT 
+        w.id,
+        p.age,
+        w.coins_needed,
+        w.power,
+        ROW_NUMBER() OVER (
+            PARTITION BY w.power, p.age 
+            ORDER BY w.coins_needed
+        ) as rnk
+    FROM Wands w
+    JOIN Wands_Property p ON w.code = p.code
+    WHERE p.is_evil = 0
+) ranked
+WHERE rnk = 1
+ORDER BY 
+power DESC, 
+age DESC;
